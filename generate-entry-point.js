17fd6ee1df896430c6ca1018a855fe6685e33f81
derogate <81664041+derogate@users.noreply.github.com>
2023-12-23 05:48:35 +0000

    feat: change url based on env

--- generate-entry-point.js
+++ generate-entry-point.js
@@ -3,14 +3,27 @@ import fs from 'node:fs';
 const GITHUB_REPOSITORY = process.env.GITHUB_REPOSITORY ?? '';
 const GITHUB_REPOSITORY_NO_USER = GITHUB_REPOSITORY.split('/').reverse()[0] ?? '';
 const isGithubPages = process.env.GITHUB_PAGES === "true";
+// const isGithubCodespaces = process.env.CODESPACES === "true";
 
 const findEntryHtmlFilesRecursive = () => {
     const htmlFilesPath = [];
     const items = fs.readdirSync("files", { withFileTypes: true, recursive: true })
     for (const item of items) {
         if (item && !item.isDirectory() && item.name === 'index.html') {
+
+            const getHref = () => {
+                if (isGithubPages) {
+                    return `/${GITHUB_REPOSITORY_NO_USER}/${item.path}/${item.name}`
+                }
+
+                // if (isGithubCodespaces) {
+                //     return `/${item.path}/${item.name}`
+                // }
+                return `/${item.path}/${item.name}`
+            }
+            
             htmlFilesPath.push({
-                href: `/${GITHUB_REPOSITORY_NO_USER}/${item.path}/${item.name}`,
+                href: getHref(),
                 folder: item.path.split('/')[1]
             })
         }
@@ -23,7 +36,7 @@ const json = JSON.stringify({
     GITHUB_REPOSITORY,
     GITHUB_REPOSITORY_NO_USER,
     isGithubPages,
-    env: process.env,
+    // isGithubCodespaces,
     entryPointsHtml: findEntryHtmlFilesRecursive()
 }, undefined, 4)
 console.log(json)
